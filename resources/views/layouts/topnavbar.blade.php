e16b1bece8cf5ed0bf7d488e1f252715400f3ad0
Arif Khan <arifkhanshubro@gmail.com>
2022-03-27 19:05:24 +0600

    new changes

--- resources/views/layouts/topnavbar.blade.php
+++ resources/views/layouts/topnavbar.blade.php
@@ -11,8 +11,21 @@
             </div>
             <div class="right-nav">
                 <li class="li-pos"><a href="{{ url('signup_signin') }}"><i class="fa fa-user-plus fa-fw"></i>Sign up</a></li>
-                <li class="li-pos"><a href="{{ url('signup_signin') }}"><i class="fa fa-sign-in fa-fw"></i>Login</a></li>
-                <!--&lt;!&ndash;<li><a href=""><i class="fa fa-user fa-fw"></i>User</a></li>&ndash;&gt;-->
+                {{-- <li class="li-pos"><a href="{{ url('signup_signin') }}"><i class="fa fa-sign-in fa-fw"></i>Login</a></li> --}}
+                <li>
+                    <div class="tooltip">
+                        <a href=""><i class="fa fa-user fa-fw"></i>User</a>
+                        <div class="tooltiptext">
+                            <img src="../images/user1.jpg" alt="" style="width: 230px;height: 150px;">
+                            <div class="short-info"><strong>Bruno Zanoelo</strong>,
+                                commonly known as <a href="#">BrunoZanoelo</a>,
+                                is an Bangladeshi programmer.
+                                <p>Total Points: 2234, Total Win: 43</p>
+                                <p>Total Accepted Solution: 2532</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
                 <!--&lt;!&ndash;<li><a href=""><i class="fa fa-comment-alt fa-fw"></i>Messages</a></li>&ndash;&gt;-->
                 <!--&lt;!&ndash;<li><a href=""><i class="fa fa-bell fa-fw"></i>Notafications</a></li>&ndash;&gt;-->
             </div>
